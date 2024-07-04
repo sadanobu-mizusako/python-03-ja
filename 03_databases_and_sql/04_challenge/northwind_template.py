5c78d4ad86faaa8a82c7d44ea51912377250925b
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-07-04 20:49:28 +0900

    commit for 03/04

--- 03_databases_and_sql/04_challenge/northwind_template.py
+++ 03_databases_and_sql/04_challenge/northwind_template.py
@@ -8,35 +8,61 @@ db = conn.cursor()
 
 # サプライヤーのリスト
 def list_of_suppliers(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "select suppliername from suppliers;"  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
+    results = [r[0] for r in results]
     return results
 
 # 少量の注文
 def count_small_orders(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+            select count(*)
+            from(
+                select orderid, sum(quantity) as quantity
+                from orderdetails
+                group by orderid
+                having quantity < 5
+            )
+    """  # ここにSQLクエリを書いてください
     db.execute(query)
     result = db.fetchone()
     return result[0] if result else 0
 
 # 最初の10商品
 def first_ten_products(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "select * from products limit 10;"  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
 # キーワードを含む商品
 def products_with_keyword(db, keyword):
-    query = ""  # ここにSQLクエリを書いてください
+    query = f"""
+            select * 
+            from products as a
+            left join suppliers as b
+            on a.supplierid = b.supplierid
+            where a.ProductName like "%{keyword}%";
+            """  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
 # 商品数が多いトップ5のカテゴリー
 def top_five_categories_by_product_count(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+            with top_5_tbl as (
+                select categoryid, count(*) as productcnt
+                from products
+                group by categoryid
+                order by productcnt desc
+                limit 5
+            )
+            select * from top_5_tbl as a
+            left join categories as b
+            on a.categoryid = b.categoryid
+    """  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
@@ -44,6 +70,9 @@ def top_five_categories_by_product_count(db):
 # 関数呼び出しの例 (提出する前にこれらをコメントアウトしてください)
 # print("List of Suppliers:")
 # print(list_of_suppliers(db))
+
+# print("\Count Small Orders:")
+# print(count_small_orders(db))
 
 # print("\nFirst Ten Products:")
 # print(first_ten_products(db))
